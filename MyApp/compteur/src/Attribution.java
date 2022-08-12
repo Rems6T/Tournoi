3dddc32373d84868cd236fab4f99828a5a858830
Rems6T <remi.tidda@gmail.com>
2022-08-12 17:27:33 +0200

    Attribution des équipes

--- MyApp/compteur/src/Attribution.java
+++ MyApp/compteur/src/Attribution.java
@@ -5,16 +5,40 @@ public class Attribution {
 
   static String[] tableau_participants;
 
+  // 0.1 : Nom du participant
+
+  static String[][] classement = new String[4][3];
+
+  // 0.0 : Nom des equipes
+  // 1.0 : Equipe 1
+  // 2.0 : Equipe 2
+  // 1.1 : Nom membre 1
+  // 2.1 : Nom membre 2
+  // 3.0 : Score
+
+  // static String[] equipe1;
+  // static String[] equipe2;
+
   public static void main(String[] args) {
     Scanner nbr_participants = new Scanner(System.in);
     System.out.print("Nombre de participants : ");
     int participants = nbr_participants.nextInt();
 
+    classement[0][0] = "Nom des equipes";
+    classement[1][0] = "Joueur 1";
+    classement[2][0] = "Joueur 2";
+    classement[3][0] = "Score";
+    classement[3][1] = "0";
+    classement[3][2] = "0";
+
     enregistrerParticipants(participants);
-
-    melangerTableau(tableau_participants); // voir la méthode en bas
-
+    melangerTableau();
     afficherNomParticipants();
+
+    creationEquipe();
+
+    afficherClassement(1);
+    afficherClassement(2);
   }
 
   // Enregistrement du nom des joueurs
@@ -27,29 +51,87 @@ public class Attribution {
       System.out.print("Nom du participant numéro " + j + " : ");
 
       String nom_du_participant = nom_participant.nextLine();
+
       tableau_participants[i] = nom_du_participant;
     }
   }
 
   // Melange des joueurs
-  static void melangerTableau(String[] shuffle_list) {
+  static void melangerTableau() {
     Random rnd = ThreadLocalRandom.current();
-    for (int i = shuffle_list.length - 1; i > 0; i--) {
+    for (int i = tableau_participants.length - 1; i > 0; i--) {
       int index = rnd.nextInt(i + 1);
 
-      String a = shuffle_list[index];
-      shuffle_list[index] = shuffle_list[i];
-      shuffle_list[i] = a;
+      String a = tableau_participants[index];
+      tableau_participants[index] = tableau_participants[i];
+      tableau_participants[i] = a;
     }
   }
 
-  // Afficher toutes les informations du tableau
-  static void tableauDesScores() {}
-
-  // Affiche le nom des entrées
+  // Affiche le nom des entrees
   static void afficherNomParticipants() {
     for (int i = 0; i < tableau_participants.length; i++) {
       System.out.println(tableau_participants[i]);
     }
   }
+
+  // ___________________________________________________________________________________________________________________________________
+
+  static void creationEquipe() {
+    // Noms d equipe personnalises
+    Scanner nomEquipe1 = new Scanner(System.in);
+    System.out.print("Nom equipe 1 : ");
+    String equipe1 = nomEquipe1.nextLine();
+
+    classement[0][1] = equipe1;
+
+    Scanner nomEquipe2 = new Scanner(System.in);
+    System.out.print("Nom equipe 2 : ");
+    String equipe2 = nomEquipe2.nextLine();
+
+    classement[0][2] = equipe2;
+
+    // Attribution des équipes
+    boolean premier1 = true;
+    boolean premier2 = true;
+
+    for (int i = tableau_participants.length - 1; i >= 0; i--) {
+      if ((i < (tableau_participants.length + 1) / 2)) {
+        if (premier1) {
+          classement[1][1] = tableau_participants[i];
+          premier1 = false;
+        } else {
+          classement[2][1] = tableau_participants[i];
+        }
+      } else {
+        if (premier2) {
+          classement[1][2] = tableau_participants[i];
+          premier2 = false;
+        } else {
+          classement[2][2] = tableau_participants[i];
+        }
+      }
+    }
+  }
+
+  // Afficher toutes les informations du tableau
+  static void afficherClassement(int equipe) {
+    if (equipe == 1 || equipe == 2) {
+      System.out.println(
+        "Nom de l\'equipe " +
+        equipe +
+        " : " +
+        classement[0][equipe] +
+        ". Joueurs : " +
+        classement[1][equipe] +
+        " et " +
+        classement[2][equipe] +
+        ". Avec un score actuel de : " +
+        classement[3][equipe] +
+        " points!!"
+      );
+    }
+  }
+
+  static void addPoint() {}
 }
