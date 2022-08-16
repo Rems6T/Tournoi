058da410c53f8a7328c2d35e1d0f8995daed733d
Rems6T <remi.tidda@gmail.com>
2022-08-16 12:51:13 +0200

    actions
    Demande une action puis l'effectue avec succès

--- MyApp/compteur/src/Attribution.java
+++ MyApp/compteur/src/Attribution.java
@@ -4,25 +4,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Attribution {
 
   static String[] tableau_participants;
-
-  // 0.1 : Nom du participant
-
-  static String[][] classement = new String[4][3];
-
-  // 0.0 : Nom des equipes
-  // 1.0 : Equipe 1
-  // 2.0 : Equipe 2
-  // 1.1 : Nom membre 1
-  // 2.1 : Nom membre 2
-  // 3.0 : Score
-
-  static int pointsEquipe1 = 0;
-  static int pointsEquipe2 = 0;
+  static String[][] classement;
 
   public static void main(String[] args) {
     Scanner nbr_participants = new Scanner(System.in);
     System.out.print("Nombre de participants : ");
     int participants = nbr_participants.nextInt();
+
+    int tailleClassement = (participants / 2) + 1;
+    classement = new String[4][tailleClassement];
 
     classement[0][0] = "Nom des equipes";
     classement[1][0] = "Joueur 1";
@@ -35,23 +25,30 @@ public class Attribution {
     melangerTableau();
     afficherNomParticipants();
 
-    creationEquipe();
+    creationPool(participants);
 
-    afficherClassement(1);
-    afficherClassement(2);
+    for (;;) {
+      Scanner faireAction = new Scanner(System.in);
+      System.out.print("score ou ajout point ou afficher participants : ");
+      String action = faireAction.nextLine();
 
-    addPoint(1);
-
-    afficherClassement(1);
-    afficherClassement(2);
-    addPoint(2);
-
-    afficherClassement(1);
-    afficherClassement(2);
-    addPoint(1);
-
-    afficherClassement(1);
-    afficherClassement(2);
+      switch (action) {
+        case "score":
+          descriptifEquipe();
+          break;
+        case "afficher participants":
+          afficherNomParticipants();
+          break;
+        case "ajout point":
+          int point = Attribution.points();
+          int numeroEquipe = Attribution.equipe();
+          addPoint(numeroEquipe, point);
+          break;
+        default:
+          System.out.print("Erreur de syntaxe.");
+          break;
+      }
+    }
   }
 
   // Enregistrement du nom des joueurs
@@ -90,19 +87,16 @@ public class Attribution {
 
   // ___________________________________________________________________________________________________________________________________
 
-  static void creationEquipe() {
-    // Noms d equipe personnalises
-    Scanner nomEquipe1 = new Scanner(System.in);
-    System.out.print("Nom equipe 1 : ");
-    String equipe1 = nomEquipe1.nextLine();
+  // Creation et attribution au hasard des equipes
+  static void creationPool(int nbrEquipe) {
+    for (int i = 1; (i * 2) <= nbrEquipe; i++) {
+      // Noms d equipe personnalises
+      Scanner nomEquipe = new Scanner(System.in);
+      System.out.print("Nom equipe " + i + " : ");
+      String equipe = nomEquipe.nextLine();
 
-    classement[0][1] = equipe1;
-
-    Scanner nomEquipe2 = new Scanner(System.in);
-    System.out.print("Nom equipe 2 : ");
-    String equipe2 = nomEquipe2.nextLine();
-
-    classement[0][2] = equipe2;
+      classement[0][i] = equipe;
+    }
 
     // Attribution des équipes
     boolean premier1 = true;
@@ -128,35 +122,47 @@ public class Attribution {
   }
 
   // Afficher toutes les informations du tableau
-  static void afficherClassement(int equipe) {
-    if (equipe == 1 || equipe == 2) {
-      System.out.println(
-        "Nom de l\'equipe " +
-        equipe +
-        " : " +
-        classement[0][equipe] +
-        ". Joueurs : " +
-        classement[1][equipe] +
-        " et " +
-        classement[2][equipe] +
-        ". Avec un score actuel de : " +
-        classement[3][equipe] +
-        " points!!"
-      );
-    }
+  static void descriptifEquipe() {
+    int equipe = equipe();
+    System.out.println(
+      "Nom de l\'equipe " +
+      equipe +
+      " : " +
+      classement[0][equipe] +
+      ". Joueurs : " +
+      classement[1][equipe] +
+      " et " +
+      classement[2][equipe] +
+      ". Avec un score actuel de : " +
+      classement[3][equipe] +
+      " points!!"
+    );
   }
 
-  static void addPoint(int equipe) {
-    if (equipe == 1) {
-      pointsEquipe1++;
-      String pointsEquipe1str = String.valueOf(pointsEquipe1);
-      classement[3][equipe] = pointsEquipe1str;
-    } else if (equipe == 2) {
-      pointsEquipe2++;
-      String pointsEquipe2str = String.valueOf(pointsEquipe2);
-      classement[3][equipe] = pointsEquipe2str;
-    } else {
-      System.out.println("Erreur de syntaxe.");
-    }
+  // Ajout de points
+  static void addPoint(int equipe, int points) {
+    int pointsEquipeInt = Integer.parseInt(classement[3][equipe]);
+    pointsEquipeInt += points;
+    classement[3][equipe] = String.valueOf(pointsEquipeInt);
+  }
+
+  static int points() {
+    Scanner nbr_points = new Scanner(System.in);
+    System.out.print("Nombre de points a attribuer : ");
+    int points = nbr_points.nextInt();
+
+    return points;
+  }
+
+  static int equipe() {
+    Scanner numeroEquipe = new Scanner(System.in);
+    System.out.print(
+      "Choisissez une equipe entre 1 et " +
+      (tableau_participants.length / 2) +
+      " : "
+    );
+    int equipe = numeroEquipe.nextInt();
+
+    return equipe;
   }
 }
